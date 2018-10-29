379792d86e510332930236db10c1c742e00875ab
codegreen-kh <vitaliy.komisarov@yahoo.com>
2018-10-30 00:09:03 +0200

    News page Item component was changed

--- src/Pages/News/Item.jsx
+++ src/Pages/News/Item.jsx
@@ -9,7 +9,7 @@ function Item ({data, styles, displayImg, displayBody, displayButton}) {
     return(
         <div className="item" style={styles}>
             <h3 className="item__title">{data.title}</h3>
-            <p className="item__body" style={{display: displayBody}}>{data.body}</p>
+            <p className="item__body" style={{display: displayBody}}>{data.body.split(/\s+/).slice(0,80).join(" ") + '...'}</p>
             <a href={data.url} style={{display: displayImg}}><img src={data.imageurl} alt={data.title}/></a>
             <button className="item__readFull" style={{display: displayButton}}><a href={data.url}>read full</a></button>
         </div>
