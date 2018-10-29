80df3f20207550a0f98a4e697eff03dcbc70c8c4
codegreen-kh <vitaliy.komisarov@yahoo.com>
2018-10-29 18:58:43 +0200

    News page was done

--- src/HOC/LogWithProps.jsx
+++ src/HOC/LogWithProps.jsx
@@ -11,12 +11,12 @@ function LogWithProps (WrappedComponent) {
 
         giveNewProps = () => {
             if (this.props.iterator === 0) {
-                this.setState ({styles: {backgroundColor: "red", width: "100%", height: " 250px", margin: "0px", textAlign: "left"}});
+                this.setState ({styles: {backgroundColor: "#b287a1", width: "1014px", height: " 250px", margin: "0px", textAlign: "left"}});
                 this.setState ({displayImg: "none"});
                 this.setState ({displayBody: "block"});
                 this.setState ({displayButton: "inline-block"});
             } else if (this.props.iterator > 0 && this.props.iterator < 3) {
-                this.setState ({styles: {backgroundColor: "green", width: "500px", margin: "6px", height: "350px", float: "left", textAlign: "left"}});
+                this.setState ({styles: {backgroundColor: "#d1b1c4", width: "490px", margin: "6px", height: "350px", float: "left", textAlign: "left"}});
                 this.setState ({displayImg: "none"});
                 this.setState ({displayBody: "block"});
                 this.setState ({displayButton: "inline-block"});
